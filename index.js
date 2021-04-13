2250df69127c56b015325e2ec3e69dd816f81a61
Andy <tty09183@gmail.com>
2021-04-13 16:19:16 +0800

    practice for my vue

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
+//vue 3.0寫法
+const AttributeBinding = {
+  data() {
+    return {
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  }
+}
+
+Vue.createApp(AttributeBinding).mount('#bind-attribute')
+
+
+//vue 2.0 寫法
+
 var app = new Vue({
     el: '#app',
     data: {
-      seen: true
+      msg:"of_couse_lee",
+    },
+  });
+
+var app1 = new Vue({
+  el: '#app02',
+  data: {
+    number:5,
+    fontColor:"red",
+    styleObject: {
+      color: 'red',
+      fontSize: '2em'
     }
-  })
+  },
+});
+
+var app3 = new Vue ({
+  el: '#app03',
+  data:{
+    fontColor:"red",
+    counter:0
+  },
+  methods: {
+    add: function(event){
+      this.counter += 1;
+    }
+  }
+  
+});
+
+var app4 = Vue.createApp({
+  el: '#example-3',
+  methods: {
+    say(message) {
+      alert(message)
+    }
+  }
+}).mount('#inline-handler')
   
 
